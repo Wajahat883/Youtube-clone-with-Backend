3a2ecb6d39385aba45c0f01c8a4be6403d1b8462
Wajahat883 <waji21569870@gmail.com>
2025-09-03 16:49:10 +0500

    feat: add getUserChannelProfile functionality and improve subscription model

--- src/Controllers/User.controller.js
+++ src/Controllers/User.controller.js
@@ -254,10 +254,77 @@ const updateUsercoverImage = asyncHandler(async (req, res) => {
     )
     return res.status(200).json(new ApiResponse(200, user, "avatar image upload successfully!"))
 })
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+    if (!username?.trim()) {
+        throw new ApiError(400, "username is missing");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscriberTo"    
+            }
+        },
+        {
+            $addFields: {
+                subscriberCount: {
+                    $size: "$subscribers"
+                },
+                channelSubscribesToCount: {   
+                    $size: "$subscriberTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$subscribers.subscriber"] }, 
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },{
+            $project:{
+                fullname:1,
+                username:1,
+                subscriberCount:1,
+                channelSubscribesToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1,
+
+
+
+            }
+
+        }
+    ]);
+    if(!channel?.length){
+        throw new ApiError(404,"channel does not exit!")
+
+    }
+    return res.status(200).json(new ApiResponse(200,channel[0],"user channel fetch successfully"))
+})
 
 export {
     registerUser, loginUser, logOutuser,
     refrehAccessToken, changeCurrentPassword,
     getCurrentUser, updateAccountDetails,
-    updateUsercoverImage, updateUserAvatar
+    updateUsercoverImage, updateUserAvatar,getUserChannelProfile
 };
--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ dotenv.config({
     path: '../env'
 })
 
+
 connectDB()
 
     .then(() => {
