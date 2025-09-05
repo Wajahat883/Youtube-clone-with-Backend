a238166b1d6c73204458d4346b882871b7b68031
Wajahat883 <waji21569870@gmail.com>
2025-09-05 11:02:37 +0500

    feat: add routes for subscriptions and videos with JWT verification for enhanced security

---
+++ src/Routes/subscriptions.routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { getSubscribedChannels, toggleSubscription, getUserChannelSubscribers } from "../Controllers/subscription.controller.js"
+import { verifyJWT } from "../middlewares/verifyJWT.js"
+
+const router = Router();
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router
+    .route("/c/:channelId")
+    .get(getSubscribedChannels)
+    .post(toggleSubscription);
+
+router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+
+export default router
---
+++ src/Routes/tweet.routes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { createTweet, getUserTweets, updateTweet, deleteTweet } from "../Controllers/tweet.controller.js"
+import { verifyJWT } from "../middlewares/verifyJWT.js"
+
+const router = Router();
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router.route("/").post(createTweet);
+router.route("/user/:userId").get(getUserTweets);
+router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+
+export default router
